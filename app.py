fbe76ba57753a34ed9fc295cc5c2b636b84dfd06
Hebin Raj <hebinraj91860@gmail.com>
2026-09-13 19:22:30 +0530

    Fix Railway Flask port

--- app.py
+++ app.py
@@ -635,4 +635,9 @@ if __name__ == "__main__":
     print(f"Connected Database: {db.DB_NAME}")
     print("Starting Hospital Appointment System Web Application...")
     print("Accessible locally at: http://127.0.0.1:5000")
-    app.run(host="127.0.0.1", port=5000, debug=True)
+
+app.run(
+    host="0.0.0.0",
+    port=int(os.environ.get("PORT", 5000)),
+    debug=False
+)
